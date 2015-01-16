78c63dba0fe271760f31f264421b139977edc7ff
dongchao <dongchao@xiaomi.com>
2015-01-17 02:56:54 +0900

    fix the store bugs

--- src/main/java/TrafficStore.java
+++ src/main/java/TrafficStore.java
@@ -29,6 +29,8 @@ public class TrafficStore {
             new HashMap<String, Integer>();
 
 
+    public static Map<String, Integer[]> bufferFlow =
+            new HashMap<String, Integer[]>();
 
 
     // ------------------- Begin Init Method ----------------------
@@ -48,8 +50,10 @@ public class TrafficStore {
             trafficLightMap.put(lightKey, lightValue);
 
             String lightNodeKey = itemString[0];
-            String[] lightNodeValue = {itemString[1], itemString[2], itemString[3], itemString[4]};
-            trafficLightNodeMap.put(lightNodeKey, lightNodeValue);
+            if (!trafficLightNodeMap.containsKey(itemString[0])) {
+                String[] lightNodeValue = {itemString[1], itemString[2], itemString[3], itemString[4]};
+                trafficLightNodeMap.put(lightNodeKey, lightNodeValue);
+            }
         }
     }
 
@@ -82,6 +86,52 @@ public class TrafficStore {
             statusHistory.get(key).get(time)[1] = statusItems.get(key)[1];
             statusHistory.get(key).get(time)[2] = statusItems.get(key)[2];
         }
+    }
+
+    public static void addFlow() {
+
+        // add the currentFlow to the bufferFlow
+        for(String key: currentFlow.keySet()) {
+
+            int flow = currentFlow.get(key);
+            int straightFlow = (int)TrafficConstant.TurnRate[2] * flow;
+            int leftFlow = (int)TrafficConstant.TurnRate[0] * flow;
+            int rightFlow = (int)TrafficConstant.TurnRate[1] * flow;
+            if(bufferFlow.containsKey(key)) {
+
+                int bufferStraight = bufferFlow.get(key)[2];
+                int bufferLeft = bufferFlow.get(key)[0];
+                int bufferRight = bufferFlow.get(key)[1];
+
+                bufferFlow.remove(key);
+                bufferFlow.put(key, new Integer[]{leftFlow+bufferLeft, rightFlow+bufferRight,
+                        straightFlow+bufferStraight});
+            }  else {
+                bufferFlow.put(key, new Integer[]{leftFlow, rightFlow, straightFlow});
+            }
+        }
+    }
+
+    public static void afterFlow(String key, Integer status[]) {
+
+        // key format: trafficId-fromId
+        Integer[] nodeFlow = bufferFlow.get(key);
+        for(int idx=0; idx<3; idx++) {
+
+            if(status[idx] == 1) {
+                // green light
+                if (idx != 2) nodeFlow[idx] -= 2;
+                else nodeFlow[idx] -= 16;
+
+                // the flow cannot < 0
+                if (nodeFlow[idx] < 0) nodeFlow[idx] = 0;
+            }
+
+
+        }
+
+        bufferFlow.remove(key);
+        bufferFlow.put(key, nodeFlow);
     }
 
     // ------------------- End Change Method
